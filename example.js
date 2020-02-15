4885a576826f161e7c50db1adf428558d6958fcb
Tom <thomas.g.nudd@durham.ac.uk>
2020-02-15 23:46:15 +0000

    file showing how to check if unlocked, include in individual .js files in ./public

---
+++ example.js
@@ -0,0 +1,28 @@
+/// how to unlock a level: change id:1 to id:1 thru id:5
+
+let rsp = await fetch("http://127.0.0.1:8090/unlock", {
+  method: "POST",
+	headers: {"Accept": "application/json",
+		"Content-Type": "application/json"},
+	body: JSON.stringify({id:1})
+});
+let data = await response.json();
+if (data && data.unlocked == true) {
+  // what do we do if we have unlocked this level?
+} else {
+  // what if we haven't?
+}
+
+
+
+//// how to check if a level is unlocked: change id=1 thru id=5
+
+let rsp = await fetch("http://127.0.0.1:8090/isUnlocked?id=1", {
+  method: "GET",
+});
+let data = await response.json();
+if (data && data.unlocked == true) {
+  // what do we do if we have unlocked this level?
+} else {
+  // what if we haven't?
+}
