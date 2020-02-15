2dbd6deac8aa746b5a9283b74ab3e91f6ac6b4c0
nguyenkim997 <nguyenkim997@hotmail.com>
2020-02-15 23:12:57 +0000

    Added Level 2 Landmark.

--- public/map.js
+++ public/map.js
@@ -1,15 +1,35 @@
+// let level = 1
+
+// if (level === 1)
 function initMap() {
-    var myLatlng = {lat: 52.408, lng: -1.507};
+
+  //Coventry Cathedral - Level 1
+    var covCath = {lat: 52.407957, lng: -1.507509};
   
     var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 15,
-        center: myLatlng
+        zoom: 20,
+        center: covCath
       });
   
     var marker = new google.maps.Marker({
-      position: myLatlng,
+      position: covCath,
       map: map,
-      title: 'Click to zoom'
+      title: 'Level 1: Coventry Cathedral'
+      });
+  
+
+    //St Mary's Guildhall - Level 2
+    var stMary = {lat:  52.407703, lng: -1.507467};
+  
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 18,
+        center: covCath
+      });
+  
+    var marker = new google.maps.Marker({
+      position: stMary,
+      map: map,
+      title: "Level 2: St Mary's Guildhall"
     });
   
     map.addListener('center_changed', function() {
@@ -21,7 +41,7 @@ function initMap() {
     });
   
     marker.addListener('click', function() {
-      map.setZoom(17);
+      map.setZoom(20);
       map.setCenter(marker.getPosition());
     });
-  }
+}
