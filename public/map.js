1450cc7bc7293cfffbb99f08e27e2e77036a13d7
nguyenkim997 <nguyenkim997@hotmail.com>
2020-02-16 11:05:13 +0000

    redesigned home page, edited css

--- public/map.js
+++ public/map.js
@@ -3,16 +3,7 @@ let levels = {1: ['Coventry Cathedral', {lat: 52.407957, lng: -1.507509}, 20,'<d
 '</div>'+
 '<h1 id="firstHeading" class="firstHeading">Coventry Cathedral</h1>'+
 '<div id="bodyContent">'+
-'<p>Coventry Cathedral, also referred to as <b>Ayers Rock</b>, is a large ' +
-'sandstone rock formation in the southern part of the '+
-'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
-'south west of the nearest large town, Alice Springs; 450&#160;km '+
-'(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
-'features of the Uluru - Kata Tjuta National Park. Uluru is '+
-'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
-'Aboriginal people of the area. It has many springs, waterholes, '+
-'rock caves and ancient paintings. Uluru is listed as a World '+
-'Heritage Site.</p>'+
+'<p>The first cathedral was constructed 1095 making the cathedral over 1000 years old. It’s first full structure was largely built  in the late 14th to earth 15th century.  During the second world war the Cathedral was almost destroyed in the Coventry Blitz of November 1940, the spire of the original cathedral still stands today. Coventry cathedral is made up of three cathedrals, showing 3 different architectural styles. The first was St Mary’s (a monastic building),  the second was St Michael’s (14th century gothic church and the last is new St Michael’s built (Regional modern – built in between 1956-1962).</p>'+
 '<p><b>Challenge: </b> You will be doing a treasure hunt, with a goal to meet another challenger!</p>'+
 '<p>Read more: Coventry Cathedral\'s <a href="https://app.slack.com/client/TSL5FDQG3/GU23NPR5K/thread/CSMF1RM25-1581806284.056600">'+
 'wikipedia.</a> </p>'+
@@ -134,6 +125,6 @@ function initMap(id) {
 }
 
 window.onload = function() {
-  const level = 2;
+  const level = 1;
   initMap(level);
 }
